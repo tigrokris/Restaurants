849bc65bb4f007c6e9f6946cbd2d10f4d7b05a55
Dmitriy <demianko@mail.ru>
2017-04-08 18:07:52 +0300

    TestUtil removed

---
+++ src/test/java/ru/demianko/restaurants/TestUtil.java
@@ -0,0 +1,41 @@
+package ru.demianko.restaurants;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import ru.demianko.restaurants.matcher.ModelMatcher;
+import ru.demianko.restaurants.model.User;
+
+import java.io.UnsupportedEncodingException;
+
+public class TestUtil {
+
+    public static ResultActions print(ResultActions action) throws UnsupportedEncodingException {
+        System.out.println(getContent(action));
+        return action;
+    }
+
+    public static String getContent(ResultActions action) throws UnsupportedEncodingException {
+        return action.andReturn().getResponse().getContentAsString();
+    }
+
+    public static RequestPostProcessor userHttpBasic(User user) {
+        return SecurityMockMvcRequestPostProcessors.httpBasic(user.getEmail(), user.getPassword());
+    }
+
+    public static void authorize(User user) {
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+    }
+
+    /**
+     * Compare entities using toString
+     */
+    public static class ToStringModelMatcher<T> extends ModelMatcher<T, String> {
+        public ToStringModelMatcher(Class<T> entityClass) {
+            super(Object::toString, entityClass);
+        }
+    }
+}
