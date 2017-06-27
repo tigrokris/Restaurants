96a3986033c818644f3f4d8736b41cf79f6c2e16
Dmitriy <demianko@mail.ru>
2017-06-27 16:34:24 +0300

    rework restaurantSelect.js

--- src/main/webapp/resources/js/restaurantSelect.js
+++ src/main/webapp/resources/js/restaurantSelect.js
@@ -3,66 +3,37 @@
  */
 var ajaxUrlRestaurants = 'ajax/profile/restaurantsList/';
 var restaurantSelect = $('#restaurantSelect');
-var restaurantsArray = [];
-/*$('#editRow').on('show.bs.modal', function () {
+
+
+function propagateSelect(dataExternal) {
     $.ajax({
             type: 'GET',
             url: ajaxUrlRestaurants,
             contentType: "application/json; charset=utf-8",
             dataType: "json",
-            async: false,
             success: function (data) {
-                restaurantSelect.empty();
+                var restaurantsArray = [];
                 $.each(data, function (i, obj) {
                     restaurantsArray[i] = obj;
                 });
-            }
-        }
-    );
-    restaurantSelect.empty();
-    for (var i = 0; i < restaurantsArray.length; i++) {
-        restaurantSelect.append($("<option></option>").val(restaurantsArray[i].id).html(restaurantsArray[i].name));
-    }
-
-    for (var i = 0; i < restaurantsArray.length; i++) {
-        if (data != undefined && restaurantsArray[i].id == data.restaurant.id) {
-            restaurantSelect.val(data.restaurant.id);
-            break;
-        }
-        restaurantSelect.val(restaurantsArray[0].id);
-    }
-    $('#restaurantId').val(restaurantSelect.val());
-});*/
-restaurantSelect.click(function () {
-    $('#restaurantId').val(restaurantSelect.val());
-})
-function propagateSelect(data) {
-    $.ajax({
-            type: 'GET',
-            url: ajaxUrlRestaurants,
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            async: false,
-            success: function (data) {
                 restaurantSelect.empty();
-                $.each(data, function (i, obj) {
-                    restaurantsArray[i] = obj;
-                });
+                for (var i = 0; i < restaurantsArray.length; i++) {
+                    restaurantSelect.append($("<option></option>").val(restaurantsArray[i].id).html(restaurantsArray[i].name));
+                }
+
+                for (var i = 0; i < restaurantsArray.length; i++) {
+                    if (data != undefined && restaurantsArray[i].id === dataExternal.restaurant.id) {
+                        restaurantSelect.val(dataExternal.restaurant.id);
+                        break;
+                    }
+                    restaurantSelect.val(restaurantsArray[0].id);
+                }
+                $('#restaurantId').val(restaurantSelect.val());
+                restaurantSelect.click(function () {
+                    $('#restaurantId').val(restaurantSelect.val());
+                })
             }
         }
     );
-    restaurantSelect.empty();
-    for (var i = 0; i < restaurantsArray.length; i++) {
-        restaurantSelect.append($("<option></option>").val(restaurantsArray[i].id).html(restaurantsArray[i].name));
-    }
-
-    for (var i = 0; i < restaurantsArray.length; i++) {
-        if (data != undefined && restaurantsArray[i].id == data.restaurant.id) {
-            restaurantSelect.val(data.restaurant.id);
-            break;
-        }
-        restaurantSelect.val(restaurantsArray[0].id);
-    }
-    $('#restaurantId').val(restaurantSelect.val());
 }
 
